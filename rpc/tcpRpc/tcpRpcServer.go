9fdba276188abbd7ac1d57276b2423a14ecffa67
prince5460 <prince5460@126.com>
2019-09-17 10:09:30 +0800

    rpc

---
+++ rpc/tcpRpc/tcpRpcServer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type Params struct {
+	Width, Height int
+}
+type Rect struct{}
+
+func (r *Rect) Area(p Params, ret *int) error {
+	*ret = p.Width * p.Height
+	return nil
+}
+
+func (r *Rect) Perimeter(p Params, ret *int) error {
+	*ret = (p.Width + p.Height) * 2
+	return nil
+}
+
+func main() {
+	rect := new(Rect)
+	//注册一个rect服务
+	rpc.Register(rect)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":2001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		rpc.ServeConn(conn)
+	}
+
+}
